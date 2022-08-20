1deb2be05f389371cc008c80b41fd7bc936386db
Sodikov Abdullo <sodikov.ak@gmail.com>
2022-08-20 12:11:20 +0500

    checkPermission

--- app/src/main/java/sodikov/weather_simple/ui/fragments/FragmentMain.kt
+++ app/src/main/java/sodikov/weather_simple/ui/fragments/FragmentMain.kt
@@ -1,13 +1,19 @@
 package sodikov.weather_simple.ui.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import sodikov.weather_simple.databinding.FragmentMainBinding
+import sodikov.weather_simple.utilit.isPermissionGranted
 
 class FragmentMain : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private var binding: FragmentMainBinding? = null
     private val mBinding get() = binding!!
 
@@ -17,6 +23,25 @@ class FragmentMain : Fragment() {
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
